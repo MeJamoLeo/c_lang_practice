599542a0d7986f476d74cae80dceb63eeb41a15c
mejamoleo <reonarudo11919273@gmail.com>
2021-01-07 02:43:25 +0900

    :bug: (use while loop)

--- validation.c
+++ validation.c
@@ -8,12 +8,20 @@ int main(void){
   printf("半径？:");
   scanf("%d",&r);
 
-  if(r<0){
-    printf("半径は負の値になりません．\n");
-  }else{
-    s = r * r * 3.14;
-    printf("面積は%fです.\n",s);
+//  if(r<0){
+//    printf("半径は負の値になりません．\n");
+//  }else{
+//    s = r * r * 3.14;
+//    printf("面積は%fです.\n",s);
+//  }
+
+  while(r < 0){
+    printf("半径?:");
+    scanf("%d",&r);
   }
+
+  s = r * r * 3.14;
+  printf("面積は%fです．\n",s);
 
   return 0;
 }
