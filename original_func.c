4aa7d396adaad8ae4b36cb6831918a63660da6af
mejamoleo <reonarudo11919273@gmail.com>
2021-01-07 03:53:15 +0900

    :sparkles: (create original function )

---
+++ original_func.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int sum(int,int); /* プロトタイプ宣言 */
+
+int main(void){
+  sum(50,100); // 呼び出し部分
+  return 0;
+}
+
+int sum(int min,int max){
+  printf("%d\n",(min +max)*(max - min + 1)/2);
+  return 0;
+}
